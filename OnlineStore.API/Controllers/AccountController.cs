db3da75105ddb53a5bb7a7d03c916f7419dae111
Jarek Blady <jarek.blady97@gmail.com>
2022-12-22 11:18:30 +0100

    update

--- OnlineStore.API/Controllers/AccountController.cs
+++ OnlineStore.API/Controllers/AccountController.cs
@@ -26,8 +26,13 @@ namespace OnlineStore.API.Controllers
         public ActionResult Login(LoginUserDto dto)
         {
             string token = _accountService.GenerateJwt(dto);
-            //return Ok(token);
-            return Ok(dto);
+            
+            var user = new UserDto
+            {
+                Email = dto.Email,
+                Token = token
+            };
+            return Ok(user);
 
         }
     }
